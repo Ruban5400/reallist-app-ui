f869c2c8581b700055a5da9457cc62963130e327
Ruban <rubans5400@gmail.com>
2023-09-14 17:07:37 +0530

    dashboards

--- lib/Home/dashboard_list.dart
+++ lib/Home/dashboard_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reallist/Home/db_checklist.dart';
 
 // class DashboardList extends StatelessWidget {
 //   @override
@@ -114,7 +115,6 @@ class DashboardList extends StatefulWidget {
 }
 
 class _DashboardListState extends State<DashboardList> {
-  // Color mainColor =Theme.of(context).colorScheme.primary;
   final PageController _pageController =
       PageController(viewportFraction: 0.55, keepPage: true, initialPage: 1);
 
@@ -132,6 +132,7 @@ class _DashboardListState extends State<DashboardList> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.01),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(
             height: 125,
@@ -148,81 +149,90 @@ class _DashboardListState extends State<DashboardList> {
                   }
                   return Transform.rotate(
                     angle: 4 * value,
-                    child: Container(
-                      // width: MediaQuery.of(context).size.width * 0.6,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: ((context) => DB_Checklist()),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        // width: MediaQuery.of(context).size.width * 0.6,
 
-                      margin: EdgeInsets.only(left: 15, bottom: 10, top: 15),
-                      padding: new EdgeInsets.all(12.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black,
-                            blurRadius: 1.0,
-                            spreadRadius: 0.0,
-                            offset: Offset(2.0, 2.0),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        // mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            '${heads[index]}',
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w500),
-                            textAlign: TextAlign.center,
-                          ),
-                          Divider(height: 10),
-                          Row(
-                            children: [
-                              Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(Icons.checklist,
-                                          size: 18, color: Colors.grey),
-                                      Text(
-                                        'Checklist',
-                                        style: TextStyle(
-                                            fontSize: 14, color: Colors.grey),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    '123',
-                                    style: TextStyle(fontSize: 16),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              ),
-                              Spacer(),
-                              Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(Icons.my_location,
-                                          size: 18, color: Colors.grey),
-                                      Text(
-                                        'Spot',
-                                        style: TextStyle(
-                                            fontSize: 14, color: Colors.grey),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    '12',
-                                    style: TextStyle(fontSize: 16),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
+                        margin: EdgeInsets.only(left: 15, bottom: 10, top: 15),
+                        padding: new EdgeInsets.all(12.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 1.0,
+                              spreadRadius: 0.0,
+                              offset: Offset(2.0, 2.0),
+                            ),
+                          ],
+                        ),
+
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '${heads[index]}',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                              textAlign: TextAlign.center,
+                            ),
+                            Divider(height: 10),
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.checklist,
+                                            size: 18, color: Colors.grey),
+                                        Text(
+                                          'Checklist',
+                                          style: TextStyle(
+                                              fontSize: 14, color: Colors.grey),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      '123',
+                                      style: TextStyle(fontSize: 16),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                                Spacer(),
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.my_location,
+                                            size: 18, color: Colors.grey),
+                                        Text(
+                                          'Spot',
+                                          style: TextStyle(
+                                              fontSize: 14, color: Colors.grey),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      '12',
+                                      style: TextStyle(fontSize: 16),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   );
@@ -230,20 +240,15 @@ class _DashboardListState extends State<DashboardList> {
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SmoothPageIndicator(
-                controller: _pageController, // PageController
-                count: heads.length,
-                effect: SwapEffect(
-                  dotHeight: 6,
-                  activeDotColor: Theme.of(context).colorScheme.primary,
-                  type: SwapType.yRotation,
-                ), // your preferred effect
-                onDotClicked: (index) {},
-              ),
-            ],
+          SmoothPageIndicator(
+            controller: _pageController, // PageController
+            count: heads.length,
+            effect: SwapEffect(
+              dotHeight: 6,
+              activeDotColor: Theme.of(context).colorScheme.primary,
+              type: SwapType.yRotation,
+            ), // your preferred effect
+            onDotClicked: (index) {},
           ),
         ],
       ),
---
+++ lib/Home/db_spots.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:reallist/Home/db_checklist.dart';
+
+class DB_Spots extends StatelessWidget {
+  const DB_Spots({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 25),
+        child: Column(
+          children: [
+            Container(
+              color: Theme.of(context).colorScheme.primary,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      size: 22,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DB_Checklist(),
+                        ),
+                      );
+                    },
+                  ),
+                  Text(
+                    'SPOTS',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.refresh,
+                      size: 24,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.all(8),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(
+                            width: 1,
+                          ),
+                        ),
+                        prefixIcon: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.close,
+                              size: 25,
+                            )),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            Icons.search,
+                            size: 25,
+                          ),
+                          onPressed: () {},
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.done,
+                    ),
+                    for (int i = 0; i < 3; i++)
+                      Container(
+                        width: double.infinity,
+                        height: 50,
+                        padding: EdgeInsets.all(8),
+                        margin: EdgeInsets.only(top: 15),
+                        alignment: Alignment.centerLeft,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(1),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              // color: Colors.black,
+                              blurRadius: 1.0,
+                              spreadRadius: 0.0,
+                              offset: Offset(2.0, 2.0),
+                            ),
+                          ],
+                        ),
+                        child: Text(
+                          'Spots',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
