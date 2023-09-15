b3ada221671f9c17a5ce90107e10b50468ed6b71
Ruban <rubans5400@gmail.com>
2023-09-15 18:55:59 +0530

    spots

--- lib/Home/spots.dart
+++ lib/Home/spots.dart
@@ -63,7 +63,7 @@ class SpotList extends StatelessWidget {
                     // margin: EdgeInsets.all(20),
                     height: MediaQuery.of(context).size.height * 0.35,
                     decoration: new BoxDecoration(
-                        color: Colors.transparent,
+                        color: Color(0xFFffffff).withOpacity(1),
                         borderRadius: new BorderRadius.all(
                           Radius.circular(30),
                         )),
